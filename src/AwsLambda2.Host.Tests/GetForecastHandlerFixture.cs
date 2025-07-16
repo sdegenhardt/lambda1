9aef01ceec7a4126e26cfa6f9fe68b5f824ae96b
Stephen Degenhardt <sdegenhardt13@outlook.com>
2025-07-16 16:56:40 -0500

    additional test

--- src/AwsLambda2.Host.Tests/GetForecastHandlerFixture.cs
+++ src/AwsLambda2.Host.Tests/GetForecastHandlerFixture.cs
@@ -1,9 +1,22 @@
-﻿namespace AwsLambda2.Host.Tests;
+﻿using UseCases.Abstractions;
+
+namespace AwsLambda2.Host.Tests;
 
 public class GetForecastHandlerFixture
 {
     [Fact]
-    public void Test1()
+    public async Task GetForcasts()
+    {
+        // Arrange
+        var sut = new GetForecastHandler();
+        var result = await sut.Handle(NoQuery.Instance, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(5, result.Length);
+    }
+
+    [Fact]
+    public void ResponseCovertsToF()
     {
         // Arrange
         var x = new GetWeatherForecastResponse(new DateOnly(2025, 7, 11), 36, "Chicago - Balmy");
